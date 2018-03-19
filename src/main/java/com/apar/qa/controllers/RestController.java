cc750b01b1e81f01acca2749d7178de26cb2e3b4
Ajay Jain <ajay.jain@apar.com>
2018-03-19 17:00:34 +0530

    By Ajay Jain

--- src/main/java/com/apar/qa/controllers/RestController.java
+++ src/main/java/com/apar/qa/controllers/RestController.java
@@ -1,6 +1,7 @@
 package com.apar.qa.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,15 +9,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.apar.qa.daoImpl.RequestBeanDaoImpl;
+
 @Controller
 @RequestMapping(value = "/ajay/", produces = MediaType.APPLICATION_JSON_VALUE)
 public class RestController {
+	
+	@Autowired
+	private RequestBeanDaoImpl requestBeanDaoImpl;
 	
 	
 	@RequestMapping(value = "/students/{studentId}/courses", method = RequestMethod.GET)
 	@ResponseBody
 	public String retrieveCoursesForStudent(@PathVariable String studentId) {
 		System.out.println("Student Id: "+studentId);
+		return "{\"Name\": \"Ajay Jain\"}";
+	}
+	
+	@RequestMapping(value = "/requests/{studentId}", method = RequestMethod.GET)
+	@ResponseBody
+	public String addUserRequest(@PathVariable String studentId) {
+		System.out.println("Student Id: "+studentId);
+		requestBeanDaoImpl.saveRequestBean();
 		return "{\"Name\": \"Ajay Jain\"}";
 	}
 	
